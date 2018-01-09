77193ece9cdad1cdce087c911cb9da3d58535827
alreadyj <kjm71712@naver.com>
2018-01-10 00:10:21 +0900

    first commit and init project

---
+++ src/main/java/common/first/web/MainController.java
@@ -0,0 +1,26 @@
+package common.first.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class MainController {
+
+    protected Logger logger = Logger.getLogger(this.getClass());
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String Index(Model model) throws Exception {
+        logger.info("index.....");
+        System.out.println("index");
+        return "main";
+    }
+    @RequestMapping("/main.jsp")
+    public String Main(Model model) throws Exception {
+        logger.info("main.....");
+        System.out.println("main");
+        return "home";
+    }
+}
